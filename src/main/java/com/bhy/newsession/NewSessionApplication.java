fe27201197d775519108693a37568d27458badc2
bewhy <baihuaiyu@ynlcgroup.com>
2023-07-07 18:02:18 +0800

    test push

--- src/main/java/com/bhy/newsession/NewSessionApplication.java
+++ src/main/java/com/bhy/newsession/NewSessionApplication.java
@@ -14,6 +14,7 @@ public class NewSessionApplication extends SpringBootInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        //
         return builder.sources(NewSessionApplication.class);
     }
 
